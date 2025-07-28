8471479dca8a1aa0d0ae9b200f0259ea9b93c382
owomed <owomedsunucu@gmail.com>
2025-07-28 18:57:26 +0300

    çekiliş.js

--- commands/çekiliş.js
+++ commands/çekiliş.js
@@ -5,7 +5,7 @@ module.exports = {
     async execute(client, message, args) {
         const allowedRoleIDs = ['1238598132745506856', '1188422639845396500']; // Komutu kullanabilecek rollerin ID'leri
         const mentionRoleID = '1238180308608749689'; // Etiketlenecek rol ID'si
-        const allowedChannels = ['1238418256490926100', '1238045966708248646', '1238416293892194416']; // İzin verilen kanal ID'leri
+        const allowedChannels = ['1238418256490926100', '1238045966708248646', '1238416293892194416','1399417189756309514']; // İzin verilen kanal ID'leri
 
         // Kullanıcının gerekli rollerden birine sahip olup olmadığını kontrol edin
         if (!message.member || !message.member.roles.cache.some(role => allowedRoleIDs.includes(role.id))) {
